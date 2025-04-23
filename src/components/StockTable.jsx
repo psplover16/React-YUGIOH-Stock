58b861b059c6f9f2d784116a3a8bdd0983dbe07b
gary_lin <gary_lin@cmoney.com.tw>
2025-04-23 13:58:36 +0800

    table 組件抽離

---
+++ src/components/StockTable.jsx
@@ -0,0 +1,72 @@
+import classNames from "classnames";
+import { Button } from "@headlessui/react";
+
+export default function StockTable({
+  theadData, // STOCK_TITLE
+  emptyTheadTd, // setSearchStr
+  //   <input
+  //   type="text"
+  //   placeholder="搜索欄位"
+  //   className="bg-amber-50"
+  //   onChange={(e) => setSearchStr(e.target.value)}
+  // />
+  tbodyData, // filterStockData
+  
+  noWrapKey,
+  titleKey,
+  formatTdWord,
+  editItem,
+}) {
+  return (
+    <table className="min-w-full border border-gray-300">
+      <thead>
+        <tr className="bg-gray-200">
+          {theadData.map((val, key) => (
+            <th
+              className="border border-gray-300 px-4 py-2 text-nowrap"
+              key={key}
+            >
+              {val.name}
+            </th>
+          ))}
+          <th className="border border-gray-300 p-2">{emptyTheadTd}</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tbodyData?.map((allVal, allKey) => (
+          <tr
+            className={allKey % 2 === 0 ? "bg-white" : "bg-gray-100"}
+            key={allKey}
+            onClick={() => editItem(allVal.id)}
+          >
+            {theadData.map((stockVal, stockKey) => (
+              <td
+                className={classNames("border border-gray-300 px-4 py-2", {
+                  "text-nowrap": stockVal.APIKey === "cardNumber",
+                })}
+                key={stockKey}
+                title={
+                  stockKey === 12
+                    ? formatTdWord(stockVal.APIKey, allVal[stockVal.APIKey])
+                    : undefined
+                }
+                colSpan={stockKey === 12 ? 2 : undefined}
+              >
+                <div className="flex items-center justify-between">
+                  <div
+                    className={classNames({
+                      "max-w-[230px] truncate": stockKey === 12,
+                    })}
+                  >
+
+                    {formatTdWord(stockVal.APIKey, allVal[stockVal.APIKey])}
+                  </div>
+                </div>
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
--- src/pages/Home/Stock.jsx
+++ src/pages/Home/Stock.jsx
@@ -74,7 +74,7 @@ export default function Stock() {
               {val.name}
             </th>
           ))}
-          <th className="border border-gray-300 py-2">
+          <th className="border border-gray-300 p-2">
             <input
               type="text"
               placeholder="搜索欄位"
@@ -89,6 +89,7 @@ export default function Stock() {
           <tr
             className={allKey % 2 === 0 ? "bg-white" : "bg-gray-100"}
             key={allKey}
+            onClick={() => editItem(allVal.id)}
           >
             {STOCK_TITLE.map((stockVal, stockKey) => (
               <td
@@ -106,26 +107,14 @@ export default function Stock() {
                 <div className="flex items-center justify-between">
                   <div
                     className={classNames({
-                      "max-w-[180px] truncate": stockKey === 12,
+                      "max-w-[230px] truncate": stockKey === 12,
                     })}
                   >
                     {formatTdWord(stockVal.APIKey, allVal[stockVal.APIKey])}
                   </div>
-
-                  {stockKey === 12 && (
-                    <Button
-                      className="text-nowrap rounded bg-gray-600 py-2 px-4 text-sm text-white data-[hover]:bg-gray-500 data-[active]:bg-gray-700"
-                      onClick={() => editItem(allVal.id)}
-                    >
-                      編輯
-                    </Button>
-                  )}
                 </div>
               </td>
             ))}
-            {/* <td className="border border-gray-300 px-1 py-2 text-center">
-  
-            </td> */}
           </tr>
         ))}
       </tbody>
